41614db4cd51459c8ebc140e655f39169de56815
vashistkamal11 <vashistkamal11@gmail.com>
2017-11-25 12:38:30 +0530

    basic version... database yet to be added

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const {app, BrowserWindow} = require('electron')
+const electron = require('electron')
 const menu = require('./menu.js').menu
+const ipc = electron.ipcMain
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
 let win
 app.on('ready', ()=> {
-    console.log(menu.item56)
     win  = new BrowserWindow({width:600, height:800})
     win.loadURL(`file://${__dirname}/view/index.html`)
+})
+
+ipc.on('display', function(event, arg=[]){
+  console.log(arg)
 })
